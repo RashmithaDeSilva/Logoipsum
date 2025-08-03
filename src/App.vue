dd9fb69179481f4eb8f31f58500a5b6f18ffd1ff
Lahiru De Silva <lahirurashmitha00@gmail.com>
2025-08-04 01:33:49 +0530

    Add Hero section

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
 import Navbar from './components/navbar.vue';
+import HeroSection from './sections/HeroSection.vue';
 
 </script>
 
 
 <template>
     <Navbar />
-
-    <router-view />
+    <HeroSection />
 </template>
 
 
---
+++ src/sections/HeroSection.vue
@@ -0,0 +1,29 @@
+<script setup>
+import HeaderVideo from '../assets/Videos/Header Video.mp4';
+</script>
+
+<template>
+  <section class="hero-section">
+    <video autoplay muted loop playsinline class="hero-video">
+      <source :src="HeaderVideo" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  </section>
+</template>
+
+<style scoped>
+.hero-section {
+  height: calc(100vh - 5.2rem);
+  overflow: hidden;
+}
+
+.hero-video {
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
+  transform: scale(1.2);
+}
+</style>
